647742f1c99c9faf0e4d3f86f2c68a80b7c23d3c
Alexandre Menezes <amenezes031@gmail.com>
2021-08-19 15:28:20 -0300

    create updatedAt style

--- src/containers/Main/components/Panel/style.js
+++ src/containers/Main/components/Panel/style.js
@@ -5,10 +5,17 @@ export const CardPanelContentStyled = styled(CardContent)`
   display: flex;
   justify-content: space-between;
   padding: 1.5625rem;
+
+  .updatedAt {
+    display: flex;
+    gap: 0.5rem;
+    align-items: center;
+  }
 `;
 
 export const ItemStyled = styled.div`
   display: flex;
+  align-items: center;
   justify-content: space-between;
   min-width: 9.375rem;
 `;
