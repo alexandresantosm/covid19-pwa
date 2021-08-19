95d557aaaaf0559a44248684ebd345bd39ab91b6
Alexandre Menezes <amenezes031@gmail.com>
2021-08-18 22:17:16 -0300

    add page main in app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { StylesProvider } from "@material-ui/styles";
 import { CssBaseline } from "@material-ui/core";
 import { GlobalStyle } from "./common/styles/global-style";
 
+import Main from "./containers/Main";
+
 function App() {
   return (
     <StylesProvider injectFirst>
       <CssBaseline />
       <GlobalStyle />
-      <div>Welcome ;)</div>
+      <Main />
     </StylesProvider>
   );
 }
