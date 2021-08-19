833b15b148716a8ad1b3b0c95f9d0123648e8282
Alexandre Menezes <amenezes031@gmail.com>
2021-08-18 22:19:47 -0300

    export all components

---
+++ src/components/index.js
@@ -0,0 +1,18 @@
+import { Button } from "./Button";
+import { Card, CardContent } from "./Card, ";
+import { Grid } from "./Grid";
+import { MenuItem } from "./MenuItem";
+import { Select } from "./Select";
+import { Typography } from "./Typography";
+import { Skeleton } from "./Skeleton";
+
+export {
+  Button,
+  Card,
+  CardContent,
+  Grid,
+  MenuItem,
+  Select,
+  Typography,
+  Skeleton,
+};
